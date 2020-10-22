0dde2111b76949d9a0922961e7ee3877dea06244
Mondhor <mondhorchihi@aol.fr>
2020-10-22 15:33:33 +0200

    AJOUT DU MAIN MENU + BOUTON RETOUR
    Le bouton Retour est dans toutes les scènes actuelles, et devra être dans toutes les futures scènes

---
+++ Assets/Scenes/MenuRelated/ReturnScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnScript : MonoBehaviour
+{
+
+    public Button Return;
+
+    public void ReturnButton()
+    {
+        Debug.Log("oui");
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+}
